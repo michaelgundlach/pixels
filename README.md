cf5dc060d4987f22a363d3474fca5efaf16d68d2
Michael Gundlach <gundlach@gmail.com>
2016-11-29 20:18:49 -0500

    Readme

---
+++ README.md
@@ -0,0 +1,13 @@
+#About#
+
+I was standing in the shower looking at the different shades of floor tiles.
+
+I wanted to describe them with a math function, and started thinking of the different functions I could display on tiles.
+
+I made this toy to fill in a 2d canvas, with a function determining the color of each pixel.
+
+--
+
+#Demo#
+
+http://htmlpreview.github.io/?https://github.com/michaelgundlach/pixels/blob/master/index.html
--- pixels.js
+++ pixels.js
@@ -1,25 +1,118 @@
-function initTable(canvas, cols, rows, func) {
-  for (var x=0; x<cols; x++) {
-    var tr = $("<tr>");
-    for (var y=0; y<rows; y++) {
-      $("<td>").css("background-color", func(x, y)).appendTo(tr);
+// Number of tiles wide and high to draw.  Actual screen width and height are
+// in .html file's <style> tag.
+const WIDTH = 100;
+const HEIGHT = 100;
+
+function twochars(n) {
+  if (n < 10) {
+    return "0" + n;
+  }
+  else {
+    return n;
+  }
+};
+
+// Returns true if graphing the function f, mapping the given domain and range
+// onto our canvas, would intersect the pixel at (x,y).
+function intersects(f, x, y, domain, range) {
+  // Map |n|, who used to live in the range [0..oldMax], to [newMin..newMax].
+  var scale = (n, oldMax, newMin, newMax) => n / oldMax * (newMax - newMin) + newMin;
+
+  var oldx = x, oldy = y;
+  var x     = scale(oldx,   WIDTH, domain[0], domain[1]);
+  var nextX = scale(oldx+1, WIDTH, domain[0], domain[1]);
+  var y     = scale(oldy,   HEIGHT, range[0], range[1]);
+  var nextY = scale(oldy+1, HEIGHT, range[0], range[1]);
+
+  var leftVal = f(x);
+  var rightVal = f(nextX);
+  if (
+    // Option 1: wave crosses left edge of pixel
+    (y <= leftVal && leftVal <= nextY) ||
+    // Option 2: wave crosses bottom of pixel
+    (y > leftVal && y < rightVal) ||
+    // Option 3: wave crosses top of pixel
+    (y < leftVal && y > rightVal) ||
+    // Option 4: wave crosses right edge of pixel
+    (y <= rightVal && rightVal <= nextY)
+    ) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function grapher(func, domain, range) {
+  return function(x, y) {
+    if (intersects(func, x, y, domain, range)) {
+      return "black";
+    } else {
+      return "white";
     }
+  };
+}
+
+// Each of these is displayed on the page as buttons
+var painters = {
+  checkerboard: (x, y) => ((x+y) % 2 ? "black" : "white"),
+
+  whitenoise: (x,y) => `rgb(${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)})`,
+
+  boring: (x, y) => "#" + twochars(x) + twochars(y) + "00",
+
+  sin_x: grapher(Math.sin, [0, 2*Math.PI], [-1, 1]),
+  cos_x: grapher(Math.cos, [0, 2*Math.PI], [-1, 1]),
+  tan_x: grapher(Math.tan, [0, 2*Math.PI], [-100, 100]),
+  polynomial: grapher((x) => 2*x*x*x-40*x*x-20*x-3, [-50,50], [-10000,10000]),
+
+  dotty_sin_x: function(x, y) {
+    if (Math.round(Math.sin(x*1.0 / WIDTH*2 * Math.PI) * HEIGHT/2 + (HEIGHT/2)) == y) {
+      return "black";
+    }
+    else {
+      return "white";
+    }
+  },
+};
+
+// Fill |canvas| calling |func(x,y)| to get the color value of each pixel.
+// Create a stylesheet denoting each pixel's color.
+function draw(canvas, func) {
+  var css = [];
+  for (var x=0; x<WIDTH; x++) {
+    for (var y=0; y<HEIGHT; y++) {
+      css.push(`#t${x}-${y} { background-color: ${func(x,y)}; }`);
+    }
+  }
+
+  $("#the_style").remove();
+  $("<style>", { id: "the_style" }).
+    text(css.join('\n')).
+    appendTo("head");
+}
+
+function initTable(canvas) {
+  $(canvas).html("");
+  for (var y=HEIGHT-1; y>=0; y--) {
+    var tr = $("<tr>");
+    for (var x=0; x<WIDTH; x++) {
+      var td = $("<td>", {id: `t${x}-${y}`});
+      td.appendTo(tr);
+    }
+
     tr.appendTo(canvas);
   }
 }
 
+// Create a button for each function that can draw on the canvas.
+function addOptionButtons(canvas, optionsDiv) {
+  Object.keys(painters).forEach(function(key) {
+    $("<button>").text(key).click(() => draw(canvas, painters[key])).appendTo(optionsDiv);
+ });
+}
+
 $(function() { 
-  var canvas = document.querySelector("#canvas");
-  var twochars = function(n) {
-    if (n < 10) {
-      return "0" + n;
-    }
-    else {
-      return n;
-    }
-  };
-  var func = function(x, y) {
-    return "#" + twochars(x) + twochars(y) + "88";
-  };
-  initTable(canvas, 100, 100, func);
+  initTable(document.querySelector("#canvas"));
+  addOptionButtons(document.querySelector("#canvas"),
+                   document.querySelector("#options"));
 });
